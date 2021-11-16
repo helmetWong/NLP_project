78fe81d60a8e228770b86cec13a4c6e01f33fd2e
Micheal Wong <63725876+helmetWong@users.noreply.github.com>
2021-11-17 00:08:20 +0800

    Add files via upload

---
+++ tweets_sentiment_analysis_train.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+from sklearn import *
+from scipy import stats
+import csv
+import time
+import pickle
+np.random.seed(100)
+
+def read_training_data(fname):
+    txtdata = []
+    classes = []
+    topics  = []
+    with open(fname, 'r', encoding='UTF-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in reader:
+            # get the text
+            txtdata.append(row[0])
+            # get the class (convert to integer)
+            if len(row)>1:
+                classes.append(row[1])
+    
+    if (len(classes)>0) and (len(txtdata) != len(classes)):        
+        raise Exception("mismatched length!")
+    
+    return (txtdata, classes)
+
+
+(traintxt, trainY) = read_training_data("train.csv")
+t1 = time.perf_counter()
+print ("start....")
+print(len(traintxt))
+
+# Bag-of-Words representation
+
+cntvect = feature_extraction.text.CountVectorizer(stop_words='english', max_features=108000)
+
+# create the vocabulary
+count_vect = cntvect.fit(traintxt)
+
+# calculate the vectors for the training data
+trainXbow = cntvect.transform(traintxt)
+
+# SVM with RBF kernel
+paramgrid = {'C': np.logspace(-2,3,20), 'gamma': np.logspace(-4,3,20) }
+print(paramgrid)
+
+svmrbf = model_selection.GridSearchCV(svm.SVC(kernel='rbf'), paramgrid, cv=6, n_jobs=8, verbose=True)
+
+model = svmrbf.fit(trainXbow, trainY);
+
+# Save the vectorizer
+vec_file = 'vectorizer.pickle'
+pickle.dump(count_vect, open(vec_file, 'wb'))
+
+# Save the model
+mod_file = 'classification.model'
+pickle.dump(model, open(mod_file, 'wb'))
+
+t2 = time.perf_counter()
+print(f'Finished in {t2-t1} seconds')  
+
---
+++ tweets_sentiment_predict_and_plot_pie.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+import os
+import csv
+import pandas as pd
+import numpy as np
+from sklearn import *
+from scipy import stats
+import pickle
+
+def read_csv_data(fname):
+    txtdata = []
+    classes = []
+    with open(fname, 'r', encoding='UTF-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in reader:
+            # get the text
+            txtdata.append(row[1])
+    
+    return (txtdata, classes)
+
+def write_csv_kaggle_sub(fname, Y):
+
+    tmp = [['Id', 'Prediction']]
+    
+    # add ID numbers for each Y
+    for (i,y) in enumerate(Y):
+        tmp2 = [(i+1), y]
+        tmp.append(tmp2)
+        
+    # write CSV file
+    with open(fname, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(tmp)
+
+cntvect = pickle.load(open('vectorizer.pickle', 'rb'))
+model = pickle.load(open('classification.model', 'rb'))
+
+path = ""
+test = "overall"
+filename = "predict2_" + test + ".csv"
+
+(testtxt, _)       = read_csv_data("result_" + test + ".csv")
+
+testXbow = cntvect.transform(testtxt)
+predY = model.best_estimator_.predict(testXbow)
+
+
+write_csv_kaggle_sub(filename, predY)
+
+df = pd.read_csv(os.path.join(path, filename))
+
+freq = df.groupby(df['Prediction']).count().unstack()
+print (freq)
+label = ["neutral", "negative", "positive"]
+explode = [0, 0, 0.15]
+
+plt.pie(freq, labels = label, explode = explode,
+        autopct='%1.1f%%',
+        wedgeprops={'edgecolor': 'black'})
+plt.title(test.upper())
+plt.show()
+
+
+
